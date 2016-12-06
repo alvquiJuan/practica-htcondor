0ea0701778967a3ffec30d59930c163890d97833
Juan Manuel Álvarez Quiñonez <juan.alvarez.quinonez@correounivalle.edu.co>
2016-12-05 23:24:16 -0500

    Create seno.c

---
+++ seno.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define  PI 3.141592
+
+int main(void)
+
+{ double X,Y;
+  char buffer[100];
+    FILE *f1;
+    FILE *f2;
+f1 = fopen("numero.txt","r");
+fread (buffer, 1, 100, f1);
+Y = atof(buffer);
+fclose(f1);
+
+X = sin (pow ((PI/('Y')),2));
+f2 = fopen("seno.txt","w");
+fprintf(f2,"%lf",X);
+fclose(f2);
+}
